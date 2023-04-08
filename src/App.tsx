a758c30977e03d9a1315c4c3efd5f02fc231f6bc
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-08 16:00:05 +0900

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { fetchAllPokemon, fetchPokemonDetail } from "./utils/pokemon";
 import { Pokemon, PokemonDetails } from "./types/pokemon";
 import { PokemonCard } from "./components/organisms/pokemon/PokemonCard";
+import { Navbar } from "./components/organisms/Navbar";
 
 const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
@@ -31,18 +32,20 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Pokemon</h1>
-      {loading ? (
-        <h2>Loading...</h2>
-      ) : (
-        <div className="pokemonCardContainer">
-          {pokemonData.map((pokemon, index) => {
-            return <PokemonCard key={index} pokemon={pokemon} />;
-          })}
-        </div>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : (
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, index) => {
+              return <PokemonCard key={index} pokemon={pokemon} />;
+            })}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
